0968c0dd574c4ea4307ec2af3b793f2f91f0cfa1
sonorangirl <manahan.erin@gmail.com>
2017-04-24 10:22:01 -0700

    Fixed mixed variable names

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ var sendHttpRequest = require('request');
 // Load environment variables.
 // Notify the developer if API credentials can't be found.
 var dotenv = require('dotenv').config();
-if (dotenv.parsed.SLACK_CLIENT_ID === '' && process.env.SLACK_CLIENT_ID === undefined) {
+if (dotenv.parsed.SLACK_CLIENT_ID === '' || process.env.SLACK_CLIENT_ID === undefined) {
   console.error('SLACK_CLIENT_ID is not set')
 }
-if (dotenv.parsed.SLACK_CLIENT_SECRET === '' && process.env.SLACK_CLIENT_SECRET === undefined) {
+if (dotenv.parsed.SLACK_CLIENT_SECRET === '' || process.env.SLACK_CLIENT_SECRET === undefined) {
   console.error('SLACK_CLIENT_SECRET is not set')
 }
 
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Construct OAuth handshake URL with API credentials.
-var hostAuthUrl = encodeURIComponent(path.resolve(process.env.HOSTNAME, 'auth'));
+var hostAuthUrl = encodeURIComponent(path.resolve(process.env.HOST_URL, 'auth'));
 var baseUrl = 'https://slack.com/api/oauth.access'
   + '?client_id=' + process.env.SLACK_CLIENT_ID
   + '&client_secret=' + process.env.SLACK_CLIENT_SECRET
@@ -62,7 +62,7 @@ app.get('/auth', function(request, response) {
   	return response.redirect('/');
   }
   // Attach the OAuth handshake code to base API URL.
-  var authUrl = baseUrl + '&code=' + req.query.code;
+  var authUrl = baseUrl + '&code=' + request.query.code;
   // request lib signature: .get([urlstring], [function callback(error, result){}])
   sendHttpRequest.get(authUrl, handleAuthResponse(response));
 });
